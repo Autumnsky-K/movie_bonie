c2cafc2d45db78e65655c988d737aa26891ac00b
Seungmin Kang <autumnsky1562@gmail.com>
2025-09-03 13:16:22 +0900

    GoRouter 프로바이더 초기 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+final routerProvider = Provider<GoRouter>((ref) {
+  return GoRouter(
+    initialLocation: '/', // 앱이 처음 시작될 때 보여줄 경로
+    routes: [
+      // '/' 경로에 대한 설정
+      GoRoute(
+        path: '/',
+        builder: (context, state) {
+          // TODO 홈화면위젯으로 교체 필요
+          return const Scaffold(
+              body: Center(
+            child: Text('Home Screen'),
+          ));
+        },
+      ),
+    ],
+  );
+});
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+
+    return MaterialApp.router(
+      routerConfig: router,
+      title: 'Movie Bonie',
     );
   }
 }
