b7c144504e63119d6234817a85c9128cc37b21b9
Seungmin Kang <autumnsky1562@gmail.com>
2025-09-15 13:13:38 +0900

    feat: Freezed로 데이터 모델 리팩토링 - abstract class로 변경 - immutable 객체 보장 및 보일러플레이트 코드 감소로 안정성과 생산성 향상

--- lib/features/movies/domain/movie.dart
+++ lib/features/movies/domain/movie.dart
@@ -1,37 +1,18 @@
-class Movie {
-  final String title;
-  final String year;
-  final String imdbID;
-  final String type;
-  final String poster;
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  Movie({
-    required this.title,
-    required this.year,
-    required this.imdbID,
-    required this.type,
-    required this.poster,
-  });
+part 'movie.freezed.dart';
+part 'movie.g.dart';
 
-  // JSON을 Movie 객체로 변환하는 팩토리 생성자
-  factory Movie.fromJson(Map<String, dynamic> json) {
-    return Movie(
-      title: json['Title'],
-      year: json['Year'],
-      imdbID: json['imdbID'],
-      type: json['Type'],
-      poster: json['Poster'],
-    );
-  }
+@freezed
+abstract class Movie with _$Movie {
+  const factory Movie({
+    @JsonKey(name: 'Title') required String title,
+    @JsonKey(name: 'Year') required String year,
+    required String imdbID,
+    @JsonKey(name: 'Type') required String type,
+    @JsonKey(name: 'Poster') required String poster,
+  }) = _Movie;
 
-  // Movie 객체를 JSON으로 변환하는 메서드
-  Map<String, dynamic> toJson() {
-    return {
-      'Title': title,
-      'Year': year,
-      'imdbID': imdbID,
-      'Type': type,
-      'Poster': poster,
-    };
-  }
+  factory Movie.fromJson(Map<String, Object?> json) => _$MovieFromJson(json);
 }
