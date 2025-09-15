2549e7ce388c7c4354adcaae1eea21f8ae1c8a82
Seungmin Kang <autumnsky1562@gmail.com>
2025-09-15 11:20:55 +0900

    feat: Freezed를 이용해 Movie와 MovieDetail 모델 리팩토링 - Movie 및 MovieDetail 데이터 모델을 Freezed 패키지를 사용하여 리팩토링함. - 이를 통해 불변 (immutable) 객체를 보장하고, 반복적인 보일러플레이트 코드(fromJson, toJson, copyWith, ==, hashCode)를 자동 생성하여 코드 안정성과 생산성 향상.

--- lib/features/movies/domain/movie.dart
+++ lib/features/movies/domain/movie.dart
@@ -1,37 +1,56 @@
-class Movie {
-  final String title;
-  final String year;
-  final String imdbID;
-  final String type;
-  final String poster;
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  Movie({
-    required this.title,
-    required this.year,
-    required this.imdbID,
-    required this.type,
-    required this.poster,
-  });
+// 생성될 파일들을 현재 파일의 일부로 포함
+part 'movie.freezed.dart';
+part 'movie.g.dart';
 
-  // JSON을 Movie 객체로 변환하는 팩토리 생성자
-  factory Movie.fromJson(Map<String, dynamic> json) {
-    return Movie(
-      title: json['Title'],
-      year: json['Year'],
-      imdbID: json['imdbID'],
-      type: json['Type'],
-      poster: json['Poster'],
-    );
-  }
+@freezed
+class Movie with _$Movie {
+  const factory Movie({
+    // @JsonKey 어노테이션을 사용하여 JSON 필드 이름과 Dart 필드 이름을 매핑
+    @JsonKey(name: 'Title') required String title,
+    @JsonKey(name: 'Year') required String year,
+    required String imdbID,
+    @JsonKey(name: 'Type') required String type,
+    @JsonKey(name: 'Poster') required String poster,
+  }) = _Movie;
 
-  // Movie 객체를 JSON으로 변환하는 메서드
-  Map<String, dynamic> toJson() {
-    return {
-      'Title': title,
-      'Year': year,
-      'imdbID': imdbID,
-      'Type': type,
-      'Poster': poster,
-    };
-  }
+  // json_serializable이 코드를 생성할 수 있도록 fromJson 팩토리 생성자 정의
+  factory Movie.fromJson(Map<String, dynamic> json) => _$MovieFromJson(json);
+
+  // final String title;
+  // final String year;
+  // final String imdbID;
+  // final String type;
+  // final String poster;
+
+  // Movie({
+  //   required this.title,
+  //   required this.year,
+  //   required this.imdbID,
+  //   required this.type,
+  //   required this.poster,
+  // });
+
+  // // JSON을 Movie 객체로 변환하는 팩토리 생성자
+  // factory Movie.fromJson(Map<String, dynamic> json) {
+  //   return Movie(
+  //     title: json['Title'],
+  //     year: json['Year'],
+  //     imdbID: json['imdbID'],
+  //     type: json['Type'],
+  //     poster: json['Poster'],
+  //   );
+  // }
+
+  // // Movie 객체를 JSON으로 변환하는 메서드
+  // Map<String, dynamic> toJson() {
+  //   return {
+  //     'Title': title,
+  //     'Year': year,
+  //     'imdbID': imdbID,
+  //     'Type': type,
+  //     'Poster': poster,
+  //   };
+  // }
 }
